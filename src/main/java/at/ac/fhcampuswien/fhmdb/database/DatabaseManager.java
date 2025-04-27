65f89495152e6b656530f20450d6bfa59e0ca1ca
goldsteynAT <danny.093@outlook.com>
2025-04-27 13:58:34 +0200

    basic DatabaseManager class structure and DB  constants

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/DatabaseManager.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb.database;
+
+import at.ac.fhcampuswien.fhmdb.exceptions.DatabaseException;
+
+public class DatabaseManager {
+
+    // constants for the db-connection -> ensures Database.DB_URL existance and that its correct
+    public static final String DB_CONNECTION_STRING = Database.DB_URL + "fhmdb";
+    public static final String DB_USER = ""; 
+    public static final String DB_PASSWORD = "";
+
+}
