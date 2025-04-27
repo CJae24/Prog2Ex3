d7b1ffa036230f74c1cbb2f7036212204b874a84
goldsteynAT <danny.093@outlook.com>
2025-04-27 14:10:42 +0200

    implemented basic Singleton and getInstance method (structure)

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/DatabaseManager.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/DatabaseManager.java
@@ -1,5 +1,7 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import com.j256.ormlite.support.ConnectionSource;
+
 import at.ac.fhcampuswien.fhmdb.exceptions.DatabaseException;
 
 public class DatabaseManager {
@@ -8,5 +10,25 @@ public class DatabaseManager {
     public static final String DB_CONNECTION_STRING = Database.DB_URL + "fhmdb";
     public static final String DB_USER = ""; 
     public static final String DB_PASSWORD = "";
+
+    // singleton
+    private static DatabaseManager instance;
+
+    // ormlite connectionsource field
+    private ConnectionSource connectionSource;
+
+    // constructor
+    private DatabaseManager() {}
+
+    // method to get one instance
+    public static synchronized DatabaseManager getInstance() throws DatabaseException {
+        if (instance == null) {
+            System.out.println("DEBUG: instance is nulll"); // temporary
+        }
+
+        // placeholder -> return null
+        return null;
+        // todo: return instance;
+    }
 
 }
