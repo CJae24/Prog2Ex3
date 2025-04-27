cb4e729a8bc0572a6f041596fd73551f983756cc
goldsteynAT <danny.093@outlook.com>
2025-04-27 22:39:39 +0200

    Adds methods to MovieRepository for creating, reading, updating, and deleting movie entities in the database cache: - addAll(List<MovieEntity>) for adding/updating movies - getAllMovies() for retrieving all cached movies - getMovieByApiId(String) for retrieving a single movie - getMoviesByIds(List<String>) for retrieving multiple movies by API ID - removeMovieById(long) for deleting by database ID - removeMovieByApiId(String) for deleting by API ID - removeAllMovies() for clearing the cache table

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
@@ -93,6 +93,10 @@ public class MovieEntity {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getApiId() {
         return apiId;
     }
